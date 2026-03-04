679c0a8df6f6cfac6ce991b9f1a4a6b0dc114356
VikentiiEfimov <efimowikenty@yandex.ru>
2026-03-04 13:16:34 +0300

    task_2.1

--- task_2.py
+++ task_2.py
@@ -3,21 +3,26 @@ class T2:
         self.my_tuple = my_tuple
     
     def run(self):
-        iterator = CyclicTupleIterator(self.my_tuple)
-        elements = []
-        for _ in range(len(self.my_tuple)):
-            elements.append(str(next(iterator)))
-        return " ".join(elements)
-
+        try:
+            iterator = CyclicTupleIterator(self.my_tuple)
+            elements = []
+            for _ in range(int(len(self.my_tuple)*2.71828)):
+                elements.append(str(next(iterator)))
+            return " ".join(elements)
+        except StopIteration:
+            return "Пустой кортеж: итерация остановлена (StopIteration)"
 
 class CyclicTupleIterator:
     def __init__(self, data: tuple):
         self.data = data
-        self.index = -1
+        self.index = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.index += 1
-        return self.data[self.index]
+        if not self.data:
+            raise StopIteration
+        value = self.data[self.index]
+        self.index = (self.index + 1) % len(self.data)
+        return value
